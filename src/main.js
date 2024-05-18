d8f88cbdecaebc66f6d8bb5278763de1b3383e24
geduramc <geduradev@gmail.com>
2024-05-18 00:48:52 -0500

    feat: create template, link buttons and config json

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import './assets/style.css'
+import './assets/bootstrap/css/bootstrap.min.css'
+import './assets/bootstrap/js/bootstrap.min.js'
+import * as config from '../public/config.json'
+
+config.links.forEach(item => {
+  const div = document.createElement('div')
+  div.classList.add('col-md-12')
+
+  const link = document.createElement('a')
+  link.classList.add('link-button')
+  link.target = "_blank"
+  link.setAttribute('rel', 'noopener noreferrer')
+
+  if (!item.enabled) {
+    link.href = '#none'
+    if (!item.enabled) link.classList.add('disable')
+  }
+  else link.href = item.url
+
+  const img = document.createElement('img')
+  img.classList.add('linkedin-icon')
+  img.src = item.icon
+
+  const span = document.createElement('span')
+  span.innerText = item.text
+
+  link.appendChild(img)
+  link.appendChild(span)
+  div.appendChild(link)
+
+  document.querySelector('.row').appendChild(div)
+})
